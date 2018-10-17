538513f5a96ccb083f8883c3314413cd7335b352
Dorian30 <alvaradogdorian@gmail.com>
2018-10-17 12:58:36 -0300

    Fourth test running

--- finger-exercises-react/react/ecmascript/01 - spread/index.js
+++ finger-exercises-react/react/ecmascript/01 - spread/index.js
@@ -1,6 +1,24 @@
 import isArray from './utils';
 
-export const min = (...args) => (isArray(args[0]) ? Math.min(...args[0]) : Math.min(...args));
+function hasNestedArray(array) {
+  return isArray(array[0]);
+}
+
+function isEmpty(array) {
+  return array.length === 0;
+}
+
+export function min(...array) {
+  if (!isEmpty(array)) {
+    if (hasNestedArray(array)) {
+      return Math.min(...array[0]);
+    }
+
+    return Math.min(...array);
+  }
+
+  return undefined;
+}
 
 export function copy() {
 
