16443e93250f73f39e7eee0636bcd52fbe9c2ce6
Dorian30 <alvaradogdorian@gmail.com>
2018-10-17 11:46:51 -0300

    Third test running

--- finger-exercises-react/react/ecmascript/01 - spread/index.js
+++ finger-exercises-react/react/ecmascript/01 - spread/index.js
@@ -1,6 +1,6 @@
 import isArray from './utils';
 
-export const min = args => (isArray(args) ? Math.min(...args) : args);
+export const min = (...args) => (isArray(args[0]) ? Math.min(...args[0]) : Math.min(...args));
 
 export function copy() {
 
