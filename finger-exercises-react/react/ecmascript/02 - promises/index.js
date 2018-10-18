4bba10390227c094e636b91e10852f82b18295b6
Dorian30 <alvaradogdorian@gmail.com>
2018-10-18 12:41:09 -0300

    Made 2nd suggested changes

--- finger-exercises-react/react/ecmascript/02 - promises/index.js
+++ finger-exercises-react/react/ecmascript/02 - promises/index.js
@@ -9,9 +9,5 @@ export function delay(time) {
 }
 
 export function asyncDelay(time) {
-  const initalTime = Date.now();
-  return new Promise((resolve, reject) => {
-    if (time > 500) reject(new Error('This time is too much !'));
-    setInterval(() => resolve(Date.now() - initalTime), time);
-  });
+  return delay(time);
 }
