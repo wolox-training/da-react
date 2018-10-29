822850e4fa3f3f3130ad93aa5dfa6dfa2eaa62b6
Dorian30 <alvaradogdorian@gmail.com>
2018-10-29 18:17:48 -0300

    Added PropTypes to Button component

--- react-app/src/app/screens/Game/components/Info/components/Button/index.js
+++ react-app/src/app/screens/Game/components/Info/components/Button/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { GO_TO_GAME_START } from '../../../../constants';
 
 function Button({ onClick, move, text }) {
   return <button onClick={() => onClick(move)}>{text}</button>;
 }
 
 export default Button;
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  move: PropTypes.number,
+  text: PropTypes.string
+};
+
+Button.defaultProps = {
+  move: 0,
+  text: GO_TO_GAME_START
+};
