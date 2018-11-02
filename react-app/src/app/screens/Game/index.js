b603804f099d41a80a17c49c6753fef7a962c286
Dorian30 <alvaradogdorian@gmail.com>
2018-11-02 12:48:23 -0300

    Took out the state and handlers from Game component

--- react-app/src/app/screens/Game/index.js
+++ react-app/src/app/screens/Game/index.js
@@ -1,67 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { calculateWinner } from './utils';
+import actionCreators from '../../../redux/game/actions';
+
 import Game from './layout.js';
 
-class GameContainer extends Component {
-  state = {
-    history: [
-      {
-        squares: Array(9).fill(null)
-      }
-    ],
-    stepNumber: 0,
-    xIsNext: true
-  };
+function GameContainer({ history, stepNumber, handleClick, jumpTo }) {
+  const current = history[stepNumber];
+  const squares = current.squares;
 
-  getConst() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-    return { history, current, winner };
-  }
-
-  handleClick = squarePosition => {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[this.state.stepNumber];
-    const squares = [...current.squares];
-    const winner = calculateWinner(squares);
-    const squareIsFilled = squares[squarePosition];
-    if (winner || squareIsFilled) return;
-    squares[squarePosition] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([
-        {
-          squares
-        }
-      ]),
-      xIsNext: !this.state.xIsNext,
-      stepNumber: history.length
-    });
-  };
-
-  jumpTo = step => {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0
-    });
-  };
-
-  render() {
-    const { history, current, winner } = this.getConst();
-    let status;
-    if (winner) status = `Winner: ${winner}`;
-    else status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    return (
-      <Game
-        history={history}
-        status={status}
-        squares={current.squares}
-        onClick={this.handleClick}
-        jumpTo={this.jumpTo}
-      />
-    );
-  }
+  return <Game history={history} status={status} squares={squares} onClick={handleClick} jumpTo={jumpTo} />;
 }
 
-export default GameContainer;
+const mapStateToProps = state => ({
+  history: state.game.history,
+  stepNumber: state.game.stepNumber
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleClick: squarePosition => dispatch(actionCreators.handleClick(squarePosition)),
+  jumpTo: move => dispatch(actionCreators.jumpTo(move))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GameContainer);
