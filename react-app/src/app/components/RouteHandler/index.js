5d26017ad67829fb19011b5f59d5a4914d7ff57d
Dorian30 <alvaradogdorian@gmail.com>
2018-11-08 10:01:50 -0300

    Minor update

--- react-app/src/app/components/RouteHandler/index.js
+++ react-app/src/app/components/RouteHandler/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Game from '../../screens/Game';
 import Login from '../../screens/Login';
