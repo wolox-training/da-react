0dfcf27009f431710e4d32733693fa42f1fc67f8
Dorian30 <alvaradogdorian@gmail.com>
2018-11-08 19:34:20 -0300

    Added empty profile user view for next card

--- react-app/src/app/components/App/index.js
+++ react-app/src/app/components/App/index.js
@@ -4,12 +4,14 @@ import { Route } from 'react-router-dom';
 import Routes from '../../../constants/routes';
 import TopBar from '../TopBar';
 import Game from '../../screens/Game';
+import UserProfile from '../../screens/UserProfile';
 
 function App() {
   return (
     <Fragment>
       <TopBar />
       <Route path={Routes.gameRoute} component={Game} />
+      <Route path={Routes.profileRoute} component={UserProfile} />
     </Fragment>
   );
 }
