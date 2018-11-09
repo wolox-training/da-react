79625b8c44349147a2a5415e199ace0c348ac658
Dorian30 <alvaradogdorian@gmail.com>
2018-11-09 17:41:21 -0300

    Fixed an error on user's profile view

--- react-app/src/app/screens/UserProfile/index.js
+++ react-app/src/app/screens/UserProfile/index.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
-function UserProfile({ Name, xWins, yWins }) {
+function UserProfile({ Name, xWins, oWins }) {
   return (
     <div className={styles.profileContainer}>
       <h2>{`Bienvenido ${Name}`}</h2>
       <div>
         <h3>Historial de Juegos:</h3>
         <p>{`- X ha ganado ${xWins} partidas`}</p>
-        <p>{`- Y ha ganado ${yWins} partidas`}</p>
+        <p>{`- O ha ganado ${oWins} partidas`}</p>
       </div>
     </div>
   );
@@ -20,7 +20,7 @@ function UserProfile({ Name, xWins, yWins }) {
 const mapStateToProps = state => ({
   Name: state.session.name,
   xWins: state.session.xWins,
-  yWins: state.session.yWins
+  oWins: state.session.oWins
 });
 
 export default connect(mapStateToProps)(UserProfile);
@@ -28,11 +28,11 @@ export default connect(mapStateToProps)(UserProfile);
 UserProfile.propTypes = {
   Name: PropTypes.string,
   xWins: PropTypes.number,
-  yWins: PropTypes.number
+  oWins: PropTypes.number
 };
 
 UserProfile.defaultProps = {
   Name: '',
   xWins: 0,
-  yWins: 0
+  oWins: 0
 };
